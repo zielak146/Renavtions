7321019d014911dc2e013bebb9ea97c39e721f94
zielak146 <125725768+zielak146@users.noreply.github.com>
2023-09-09 08:26:17 +0100

    Contact section added

--- script.js
+++ script.js
@@ -19,34 +19,9 @@ document.addEventListener('DOMContentLoaded', function () {
 	window.addEventListener('scroll', addBackground);
 });
 
-const userName = 'Marek';
 
-if (userName) {
-	console.log('Witaj ' + userName + '!');
-}
 
-if (userName !== '') {
-	console.log('Witaj ' + userName);
-}
 
-let hasTicket = false;
-let moreThan15Years = true;
-let bossFamily = true;
+ 
 
-if ((hasTicket && moreThan15Years) || bossFamily) {
-	console.log('Can Enter');
-} else {
-	console.log('Unfortunately not');
-}
-
-let age = 71;
-if (age <= 9) {
-	console.log('Child');
-} else if (age <= 18){
-  console.log('Still young!');
-} else if (age <= 70){
-  console.log('Mature');
-}else {
-  console.log('Wise');
-}
 
