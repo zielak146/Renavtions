1c2e9389927ddb415eee58d949e8dc96aaacb96d
zielak146 <125725768+zielak146@users.noreply.github.com>
2023-10-12 16:47:15 +0100

    Color change

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ const navBtn = document.querySelector('.navbar-toggler');
 const navbarItems = document.querySelector('.navbar-items');
 
 navBtn.addEventListener('click', () => {
-    navbarItems.classList.toggle('navbar-items--active');
+	navbarItems.classList.toggle('navbar-items--active');
 });
 
 // Close the navigation menu when a link is clicked
 const navLinks = document.querySelectorAll('.nav-link');
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navbarItems.classList.remove('navbar-items--active');
-    });
+navLinks.forEach((link) => {
+	link.addEventListener('click', () => {
+		navbarItems.classList.remove('navbar-items--active');
+	});
 });
 
-
-
-
-
- 
-
-
+function addOne(item){
+	item++
+	console.log(item);
+	return item;
+}
+function addAllWords(...words){
+	let txt = '';
+  for(let i = 0; i > words.lenght; i++){
+  txt += words[i]}
+  console.log(txt);
+  }
